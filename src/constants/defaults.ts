c133f534d82855aa6c94504d945f67745fbc3c84
Aureon <nausheenrazvi57@gmail.com>
2026-08-12 15:37:21 +0530

    feat(defaults): update SDK version to 0.1.2

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -9,7 +9,7 @@ export const DEFAULT_API_BASE_URL = "https://api.aureonlabs.network";
 export const LOCAL_API_BASE_URL = "http://127.0.0.1:8787";
 
 export const DEFAULT_TIMEOUT_MS = 30_000;
-export const SDK_VERSION = "0.1.0";
+export const SDK_VERSION = "0.1.2";
 export const SDK_NAME = "@buildaureon/sdk";
 export const PRODUCT_NAME = "AUREON";
 export const PRODUCT_TAGLINE =
